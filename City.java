f23e7f7d51e5bea960d7bb77af86319524637de4
dileksimalaydn <d.simalaydn@icloud.com>
2024-12-14 17:07:34 +0300

    nextCities 2d array added to city class

--- City.java
+++ City.java
@@ -1,12 +1,18 @@
 public class City {
     private String cityName;
+    private String[][] nextCities;
 
-    public City(String cityName){
+    public City(String cityName,String[][] nextCities){
         this.cityName = cityName;
+        this.nextCities= nextCities;
     }
 
     public String getCityName() {
         return cityName;
+    }
+     
+    public String[][] getNextCities(){
+        return nextCities;
     }
 
     public void setCityName(String cityName) {
