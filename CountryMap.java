613698a36803c8921f41df0982155a6f40608f32
dileksimalaydn <d.simalaydin@icloud.com>
2024-12-14 17:59:14 +0300

    Created CountryMap class

---
+++ CountryMap.java
@@ -0,0 +1,14 @@
+public class CountryMap {
+
+    private String inputMapFileName;
+    private String[] cityNames;
+    private City[] cities;
+    private String computationLine;
+
+    public CountryMap(String inputMapFileName){
+        this.inputMapFileName = inputMapFileName;
+    }
+
+
+    
+}
