c2c63a9a19dfa621e86f5e6a384c381a93295393
dileksimalaydn <d.simalaydin@icloud.com>
2024-12-17 20:39:14 +0300

    Added readInputFile and read count of city and read city names

--- CountryMap.java
+++ CountryMap.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class CountryMap {
 
     private String inputMapFileName;
@@ -16,6 +20,27 @@ public class CountryMap {
     public City[] getCities() {
         return cities;
     }
+
+    public void readInputFile()
+    {
+        try(BufferedReader reader = new BufferedReader(new FileReader(inputMapFileName)))
+        {
+            // City Count
+            String line = reader.readLine();
+            int cityCount = Integer.parseInt(line.trim());
+
+            //City Names
+            line = reader.readLine();
+            cityNames = line.trim().split(" ");
+
+            
+
+        } catch(IOException e) {
+
+        }
+    }
+
+    
 
 
 
