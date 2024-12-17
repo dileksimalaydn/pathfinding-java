c85382c72ebd85f8c66f05a4f134618720343656
dileksimalaydn <d.simalaydin@icloud.com>
2024-12-17 21:05:08 +0300

    Created WayFinder class for fastest way algorithm

---
+++ WayFinder.java
@@ -0,0 +1,12 @@
+public class WayFinder {
+    
+    private CountryMap countryMap;
+
+    public WayFinder(CountryMap countryMap) {
+        this.countryMap = countryMap;
+    }
+
+    public String findFastestPath() {
+        
+    }
+}
